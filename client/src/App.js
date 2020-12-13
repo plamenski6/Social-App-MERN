abd45170c2550dc726c9d9bb2e53dbc7e0174eb4
Plamen Shumanski <plamen1030@abv.bg>
2020-12-13 14:22:21 +0200

    feat: adding styles and redux logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 
+import { getPosts } from './actions/posts'
 import memories from './images/memories.png'
+import Posts from './components/posts/Posts'
+import Form from './components/form/Form'
+import useStyles from './styles'
 
 const App = () => {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
+
     return (
         <Container maxWidth='lg'>
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align='center'>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>
                     Social App
                 </Typography>
-                <img src={memories} alt='memories' height='60' />
+                <img className={classes.image} src={memories} alt='memories' height='60' />
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
                     </Grid>
                 </Container>
             </Grow>
