6a4ec630b96d51e3c89c243e57d114d251d1ade7
Plamen Shumanski <plamen1030@abv.bg>
2020-12-17 16:38:16 +0200

    feat: adding update feature

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -11,15 +12,26 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+    const { title, message, selectedFile, creator, tags } = req.body
 
     const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
 
     try {
-        await newPostMessage.save();
+        await newPostMessage.save()
 
-        res.status(201).json(newPostMessage);
+        res.status(201).json(newPostMessage)
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        res.status(409).json({ message: error.message })
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params
+    const post = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with id: ${_id}`)
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
+
+    res.json(updatedPost)
 }
